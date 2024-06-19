ece544f3f221e6e24c2d2541a19d53be5f8d341a
Lasidu Dilshan <126545632+LasiduDilshan@users.noreply.github.com>
2024-06-19 10:54:38 +0530

    added Digital Communication 2(same as 1 but a different approach)

---
+++ Digital Communication 2/Decrypt.py
@@ -0,0 +1,30 @@
+#python code for encryption
+
+# take path of the file as an input
+##path = r'output.txt'
+path = r'output.tmp'
+##path = r'output.wav'
+
+# taking encryption key as input
+key = 122
+		
+# open file for reading purpose
+fin = open(path, 'rb')
+	
+# storing file's data in variable "file"
+file = fin.read()
+fin.close()
+	
+# converting file into byte array to perform encryption easily on numeric data
+file = bytearray(file)
+
+# performing XOR operation on each value of bytearray
+for index, values in enumerate(file):
+        file[index] = values ^ key
+
+# opening file for writting purpose
+fin = open(path, 'wb')
+	
+# writing encrypted data in image
+fin.write(file)
+fin.close()
